0949b27f3f37012339326174cc70d74ecfa03c38
alex <alex.h.z.xu@oocl.com>
2020-07-27 15:45:16 +0800

    [alex xu] refactor: transform w1, w2 to firstWordInfo, secondWordInfo

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -32,7 +32,7 @@ public class WordFrequencyGame {
                 }
                 wordInfoList = list;
 
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo w : wordInfoList) {
